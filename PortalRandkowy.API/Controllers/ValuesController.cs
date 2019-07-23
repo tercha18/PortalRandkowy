2ca3d7ed0b6309bc012ff3552b6c38b7eba67875
Daniel Tercha <tercha.daniel18@gmail.com>
2019-07-23 23:22:27 +0200

    Controller method.

--- PortalRandkowy.API/Controllers/ValuesController.cs
+++ PortalRandkowy.API/Controllers/ValuesController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using PortalRandkowy.API.Data;
+using PortalRandkowy.API.Models;
 
 namespace PortalRandkowy.API.Controllers
 {
@@ -10,36 +12,57 @@ namespace PortalRandkowy.API.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
+        private DataContext context { get; }
+        public ValuesController(DataContext context)
+        {
+            this.context = context;
+        }
+
         // GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public IActionResult GetValues()
         {
-            return new string[] { "elo", "xxx" };
+            var values = context.Values.ToList();
+            return Ok(values);
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        public IActionResult GetValue(int id)
         {
-            return "value";
+            var value = context.Values.FirstOrDefault(x => x.Id == id);
+            return Ok(value);
         }
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody] string value)
+        public IActionResult AddValue([FromBody] Value value)
         {
+            context.Values.Add(value);
+            context.SaveChanges();
+            return Ok(value);
         }
 
         // PUT api/values/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public IActionResult EditValue(int id, [FromBody] Value value)
         {
+            var data = context.Values.Find(id);
+            data.Name = value.Name;
+            context.SaveChanges();
+            return Ok(data);
         }
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public IActionResult DeleteValue(int id)
         {
+            var data = context.Values.Find(id);
+            if (data == null)
+                return NoContent();
+            context.Values.Remove(data);
+            context.SaveChanges();
+            return Ok(data);
         }
     }
 }
