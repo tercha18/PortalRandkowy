d964f8245a61c545eedec042cb36d6b34f37edcb
Daniel Tercha <tercha.daniel18@gmail.com>
2019-07-23 22:33:52 +0200

    First model.

---
+++ PortalRandkowy.API/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PortalRandkowy.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PortalRandkowy.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
